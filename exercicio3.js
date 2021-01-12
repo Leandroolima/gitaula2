d2b6fecc897950868a966c81949f528a412217fd
leandro lima <leandro6666lima@gmail.com>
2021-01-12 16:28:19 -0300

    ex3

---
+++ exercicio3.js
@@ -0,0 +1,15 @@
+
+   // for(i=1; i<=10 ; i++){
+  //   console.log("1 x "+i+" = " + (1*i));
+ // }*
+
+
+
+ var i, j;
+ for (i = 0; i <= 10; i++) {
+     console.log("resultado")
+     for (j = 1; j <= 10; j++) {
+        console.log(i+ " x " + j +" = "+i*j);
+     }
+    
+ }
